625e4e09bd643546ab97b05568e79fd99aed3ef1
Panzerr <samir.labane42@gmail.com>
2017-01-05 15:25:46 +0100

    submap loading

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
 $arg = $_GET["path"];
-$path = "Maps/".$arg."/";
-
+$path = "Maps/".str_replace('|','/',$arg)."/";
+$arg = strchr($arg,'|');
+$arg = substr($arg,1);
 
 if ($fileContents = file_get_contents($path.$arg.".json"))
 {
 	$mapInfo = json_decode($fileContents);
 	echo '<img src="'.$path.$mapInfo->file.'">';
+	foreach ($mapInfo->submaps as &$submap)
+	{
+			echo "<div style=\"position: absolute; top:".$submap->position[0]."px;left:".$submap->position[1]."px; width:200px; height:25px\">";
+			echo $submap->name;
+			echo "</div>";
+	}
 }
 else
 	echo "wrong path";
